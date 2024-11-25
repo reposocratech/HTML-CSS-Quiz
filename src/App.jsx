0647f7db2e5f90e2ddd05698cc1644283527d67c
sergio <sjr.social@gmail.com>
2024-11-25 11:59:34 +0100

    Creados componenets Header y Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import { useContext } from 'react';
-import { QuizContext } from './context/QuizContext';
+import { Header } from './components/header/Header';
+import { Game } from './components/quiz/game/Game';
 import { Footer } from './components/footer/Footer';
-import { FinalView } from './components/finalView/FinalView';
-import { Quiz } from './components/quiz/Quiz';
 
 import './App.css';
 
 const App = () => {
-  const { completed } = useContext(QuizContext);
-
   return (
     <>
-      <main>
-        <h1>
-          <div className="img">
-            <img src="./images/favicon.png" alt="Logo de HTML-CSS-QUIZ" />
-          </div>
-          HTML & CSS QUIZ
-        </h1>
-        {completed ? <FinalView /> : <Quiz />}
-      </main>
+      <Header />
+      <Game />
       <Footer />
     </>
   );
---
+++ src/components/quiz/game/Game.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { QuizContext } from '../../../context/QuizContext';
+import { FinalView } from '../../finalView/FinalView';
+import { Quiz } from '../Quiz';
+
+export const Game = () => {
+  const { completed } = useContext(QuizContext);
+
+  return <main>{completed ? <FinalView /> : <Quiz />}</main>;
+};
